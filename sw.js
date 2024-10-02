50441398defb65073e5c82e879a334ab27c0a379
Zukisa Masiza <zukisa@turfsport.co.za>
2024-10-02 11:14:46 +0200

    WIP

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-self.addEventListener('install', event => {
+// sw.js
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('v1').then(cache => {
+        caches.open('v1').then((cache) => {
             return cache.addAll([
                 '/',
                 '/index.html',
                 '/styles.css',
                 '/app.js',
+                '/data.js',
                 '/manifest.json'
-                // Add icons if available
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(response => {
+        caches.match(event.request).then((response) => {
             return response || fetch(event.request);
         })
     );
